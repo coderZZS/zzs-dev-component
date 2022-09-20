9ee8fbe655fcb757053688e006b55e7f563d8f3b
xyj <296576733@qq.com>
2022-09-20 09:55:41 +0800

    fix: conf

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,16 @@
  * @Descripttion: 
  * @Author: BZR
  * @Date: 2022-06-21 10:55:34
- * @LastEditTime: 2022-09-19 16:43:24
+ * @LastEditTime: 2022-09-19 16:47:55
 -->
 <template>
+    <ZText />
     <router-view></router-view>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ZText } from 'zzs-dev-component'
+</script>
 
 <style>
 span {
