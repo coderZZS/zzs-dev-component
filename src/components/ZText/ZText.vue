09a8d7c091305f2f44b4ee237cc7c7f63900f16b
xyj <296576733@qq.com>
2022-09-20 10:51:32 +0800

    Keep calm and commit

--- src/components/ZText/ZText.vue
+++ src/components/ZText/ZText.vue
@@ -2,7 +2,7 @@
  * @Descripttion: 
  * @Author: BZR
  * @Date: 2022-09-19 11:43:50
- * @LastEditTime: 2022-09-19 14:19:18
+ * @LastEditTime: 2022-09-20 10:41:01
 -->
 <template>
     <component :is="props.tag" :style="styleProps" class="z-text-component" @click="handelClick">{{ props.text }}</component>
--- src/components/ZText/index.ts
+++ src/components/ZText/index.ts
@@ -2,7 +2,7 @@
  * @Descripttion:
  * @Author: BZR
  * @Date: 2022-09-19 14:16:32
- * @LastEditTime: 2022-09-19 14:19:27
+ * @LastEditTime: 2022-09-20 10:40:42
  */
 import { App } from 'vue'
 import ZText from './ZText.vue'
